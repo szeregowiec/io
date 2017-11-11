229a33ca3506747c75c872094541105ccddb6d92
Piotr <karager009@gmail.com>
2017-11-11 23:41:29 +0100

    Merge branch 'master' of https://github.com/szeregowiec/io
    # Conflicts:
    #	.idea/modules/Library_main.iml
    #	src/main/java/Application.java
    #	src/main/resources/hibernate.cfg.xml

--- src/main/java/Login/LoginController.java
+++ src/main/java/Login/LoginController.java
@@ -1,14 +1,26 @@
 package Login;
+import Base.Database;
 import DataSchema.ReadersEntity;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.List;
 
 
 public class LoginController {
 
-    public static String getId(){
-        return new ReadersEntity().getIdReader();
+    public static Route loginIfRegistered = (request, response) -> {
+        //Database database= new Database();
 
+        List reader = Main.Application.database.getSession().createQuery("FROM ReadersEntity WHERE email = :email and password = :password").setParameter("email", request.queryParams("inputEmail")).setParameter("password", request.queryParams("inputPassword")).list();
+        if(reader.isEmpty()){
+            return "You cannot log in. You have given us wrong email or password. Sorry!";
+        }else {
+            ReadersEntity re =  ((ReadersEntity) reader.get(0));
 
-    }
-
+            return "Hello "+re.getName()+" "+ re.getSurname()+". You have succesfully logged in. Your login is: "+ re.getLogin();
+        }
+    };
 
 }
